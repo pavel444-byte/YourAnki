f183f82377ac33cbc02c79b8e6f71a662de5fa85
pavel444-byte <pavel.melnikov.kid.1993@gmail.com>
2025-07-29 09:27:59 +0500

    Added the code to select topic

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import pandas as pd
 import numpy as np
 import requests
 
-english_levels = ["Beginner", "Elementary", "Intermediate", "Upper Intermediate", "Advanced", "Proficient"]
+english_levels = ["A1", "A2", "B1", "B2", "C1", "C2"]
+st.write("Note: For more infromation about english levels, please visit [this link](https://en.wikipedia.org/wiki/Common_European_Framework_of_Reference_for_Languages).")
 selected_level = st.selectbox(
     "Select your English level:",
     english_levels,
@@ -15,3 +16,12 @@ selected_level = st.selectbox(
 )
 
 st.write("You selected:", selected_level)
+
+english_topics = ["Math", "Technology", "Politics", "Family", "Kids", "Animals", "Food", "Travel", "Health", "Education"]
+selected_topic = st.selectbox(
+    "Select a topic to learn about:",
+    english_topics,
+    index=None,
+    placeholder="Choose a topic...",
+)
+st.write("You selected:", selected_topic)
